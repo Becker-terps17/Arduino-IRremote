04effe71cf79620fdfb97386293608c1b92fb362
Becker <alexander.m.b.smith@gmail.com>
2017-10-10 09:18:57 -0400

    Add files via upload

---
+++ IR_Beacon_Modified.ino
@@ -0,0 +1,77 @@
+/*
+ *ENES 100 send demo. Messages for ENES, 
+ */
+
+
+#include <IRremote.h>
+
+enum messages {ENES, KEYSTONE, OSV};
+messages message = ENES;
+int dt = 100;
+IRsend irsend;
+
+void setup()
+{
+  Serial.begin(9600);
+  message =  random(0,3);
+}
+
+void loop() {
+  message =  random(0,3);
+  switch(message){
+  case(ENES):
+//  Serial.print("ENES\n");
+  for (int i = 0; i < 3; i++) {
+    irsend.sendSony(0x45, 12);
+    delay(dt);
+    irsend.sendSony(0x4e, 12);
+    delay(dt);
+    irsend.sendSony(0x45, 12);
+    delay(dt);
+    irsend.sendSony(0x53, 12);
+    delay(dt);
+    irsend.sendSony(0x0a, 12);
+    delay(dt);
+  }
+  delay(500);
+  break; 
+  case(KEYSTONE):
+//  Serial.print("KEYSTONE\n");
+  for (int i = 0; i < 3; i++) {
+    irsend.sendSony(0x4b, 12);
+    delay(dt);
+    irsend.sendSony(0x45, 12);
+    delay(dt);
+    irsend.sendSony(0x59, 12);
+    delay(dt);
+    irsend.sendSony(0x53, 12);
+    delay(dt);
+    irsend.sendSony(0x54, 12);
+    delay(dt);
+    irsend.sendSony(0x4f, 12);
+    delay(dt);
+    irsend.sendSony(0x4e, 12);
+    delay(dt);
+    irsend.sendSony(0x45, 12);
+    delay(dt);
+    irsend.sendSony(0x0a, 12);
+    delay(dt);
+  }
+  delay(500);
+  break; 
+  case(OSV):
+//  Serial.print("OSV\n");
+  for (int i = 0; i < 3; i++) {
+    irsend.sendSony(0x4f, 12);
+    delay(dt);
+    irsend.sendSony(0x53, 12);
+    delay(dt);
+    irsend.sendSony(0x56, 12);
+    delay(dt);
+    irsend.sendSony(0x0a, 12);
+    delay(dt);
+  }
+  delay(500);
+  break; 
+  }
+}
---
+++ IR_Receive_Modified.ino
@@ -0,0 +1,38 @@
+/*
+ * Code demo for receiving IR message and decoding it
+ */
+
+#include <IRremote.h>
+
+int RECV_PIN = 11;
+int result = 0;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  // In case the interrupt driver crashes on setup, give a clue
+  // to the user what's going on.
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Enabled IRin");
+}
+
+void loop() {
+//  Serial.println(decode_message(0x45));
+  if (irrecv.decode(&results)) {
+    Serial.print(decode_message(results.value));
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
+}
+
+char decode_message(int mess){
+  int mask = 0xff;
+  int message= mess & mask;
+  return char(message);
+}
+
